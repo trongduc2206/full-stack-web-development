26d1dd9d9f9f8b4f2f992567fe73dadd2b5987df
Trong Duc <trongduc22062000@gmail.com>
2024-06-23 00:24:59 +0300

    uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+app.use(express.static('dist'))
 app.use(express.json())
 morgan.token('body', function (req, res) { if(Object.keys(req.body).length !== 0) {return JSON.stringify(req.body)} else { return " "} })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
